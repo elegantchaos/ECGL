b5499ce2f416d82d50171fb6b9880fb31ccf3007
Sam Deane <sam@elegantchaos.com>
2013-05-01 13:57:08 +0100

    temporary renaming

--- Source2/touch/ECGLTextureLink.h
+++ Source2/touch/ECGLTextureLink.h
@@ -1,9 +1,16 @@
 // --------------------------------------------------------------------------
 //! @author Sam Deane
-//! @date 21/05/2010
+//! @date 11/08/2010
 //
 //  Copyright 2010 Sam Deane, Elegant Chaos. All rights reserved.
 // --------------------------------------------------------------------------
 
-#import <ECFoundation/ECFoundationReleasePrefix.pch>
-#import "ECGLSharedPrefix.pch"
+#import <Foundation/Foundation.h>
+
+@class ECGLTexture;
+@interface ECGLTextureLink : NSObject 
+
+@property (retain, nonatomic) ECGLTexture*	texture;
+@property (assign, nonatomic) NSInteger		index;
+
+@end
